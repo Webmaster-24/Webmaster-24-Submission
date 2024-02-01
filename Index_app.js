1c1c02a32bb618050eb57c6a05a86cfa2d9d257d
Emilio Armijo <armijoemilio08@gmail.com>
2024-02-01 13:20:20 -0600

    THE FINALE B4 REGIONALS RAHHHH

--- Index_app.js
+++
@@ -1,9 +0,0 @@
-/* Set the width of the sidebar to 250px to pull out the sidebar */
-function openNav() {
-    document.getElementById("mySidepanel").style.width = "250px";
-}
-
-/* Set the width of the sidebar to 0 to hide the sidebar */
-function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-}
